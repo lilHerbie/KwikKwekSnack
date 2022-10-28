d876a5c1c5399b3565f5766f4f32e4bbab0b0df1
GijsVDVelden <gj.vandervelden1@student.avans.nl>
2022-10-28 15:22:31 +0200

    Crud screens for Drinks and Extras

--- KwikKwekSnack_ClassLibary/Drink.cs
+++ KwikKwekSnack_ClassLibary/Drink.cs
@@ -11,7 +11,7 @@ namespace KwikKwekSnack_ClassLibary
     {
         public int ID { get; set; } 
         public string Name { get; set; }
-        public string Description { get; }
+        public string Description { get; set; }
         public string ImageUrl { get; set; }
         public double StartPrice { get; set; }
     }
